78e96d919aaef4bb6d2043203f10eda3c148d75e
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-23 11:02:20 +0530

    Initial Commit

---
+++ crypto-traker/crypto_trakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_trakerApp.swift
+//  crypto-traker
+//
+//  Created by Jayesh Sadhwani on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct crypto_trakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
