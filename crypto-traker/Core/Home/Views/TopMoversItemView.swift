87255f8e5059c475b5e317d0c9871e61cdb1d0ab
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-23 11:26:20 +0530

    top movers item

--- crypto-traker/Core/Home/Views/TopMoversItemView.swift
+++ crypto-traker/Core/Home/Views/TopMoversItemView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct TopMoversItemView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (alignment: .leading) {
+            // image
+            Image(systemName: "bitcoinsign.circle.fill")
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
+                .padding(.bottom, 8)
+            
+            // coin info
+            HStack(spacing: 2) {
+                Text("BTC")
+                    .font(.caption)
+                    .fontWeight(.bold)
+                
+                Text("$20,300.00")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            
+            // coin percentage
+            Text("+ 5.06%")
+                .font(.title2)
+                .foregroundColor(.green)
+        }
+        .frame(width: 140, height: 140)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color(.systemGray4), lineWidth: 2)
+        )
     }
 }
 
