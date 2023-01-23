798b2d21385fb6920e59df3d3ffb2b6a954d468a
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-23 13:28:49 +0530

    format numbers

---
+++ crypto-traker/Extensions/Double.swift
@@ -0,0 +1,41 @@
+//
+//  Double.swift
+//  crypto-traker
+//
+//  Created by Jayesh Sadhwani on 23/01/23.
+//
+
+import Foundation
+
+extension Double {
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter
+    }
+    
+    private var numberFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter
+    }
+    
+    func toCurrency() -> String {
+        return currencyFormatter.string(for: self) ?? "$0.00"
+    }
+    
+    func toPercentString() -> String {
+        guard let numberAsString = numberFormatter.string(for: self) else { return "0.00%" }
+        return numberAsString + "%"
+    }
+}
