ca606ccdeeef3c8c481a3b5794babab52071379b
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-24 18:12:13 +0530

    detail view basic

--- crypto-traker/Core/Detail/ViewModels/DetailViewModel.swift
+++ crypto-traker/Core/Detail/ViewModels/DetailViewModel.swift
@@ -10,21 +10,89 @@ import Combine
 
 class DetailViewModel: ObservableObject {
     
-    private let coinDetailService: CoinDetailDataService
+    @Published var overviewStatistics: [Statistic] = []
+    @Published var additionalStatistics: [Statistic] = []
+    @Published var coin: Coin
     
+    private let coinDetailService: CoinDetailDataService
     private var cancellables = Set<AnyCancellable>()
     
     init(coin: Coin) {
+        self.coin = coin
         self.coinDetailService = CoinDetailDataService(coin: coin)
         self.addSubscribers()
     }
     
     private func addSubscribers() {
         coinDetailService.$coinDetails
-            .sink { returnedCoinDetails in
-                print("DEBUG: Recieved coin data")
-                print(returnedCoinDetails)
+            .combineLatest($coin)
+            .map(mapDataToStatistics)
+            .sink { [weak self] returnedArrays in
+                self?.overviewStatistics = returnedArrays.overview
+                self?.additionalStatistics = returnedArrays.additional
             }
             .store(in: &cancellables)
+        }
+    
+    private func mapDataToStatistics(coinDetails: CoinDetail?, coins: Coin) -> (overview: [Statistic], additional: [Statistic]) {
+        let overviewArray = createOverviewArray(coin: coin)
+        let additionalArray = createAdditionalArray(coin: coin, coinDetails: coinDetails)
+        
+        return (overviewArray, additionalArray)
+    }
+    
+    private func createOverviewArray(coin: Coin) -> [Statistic] {
+        let price = coin.currentPrice.toCurrency()
+        let pricePercentChange = coin.priceChangePercentage24H
+        let priceStat = Statistic(title: "Current Price", value: price, percentageChange: pricePercentChange)
+        
+        let marketCap = "$" + (coin.marketCap?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentageChange = coin.marketCapChangePercentage24H
+        let marketCapStat = Statistic(title: "Market Capitalization", value: marketCap, percentageChange: marketCapPercentageChange)
+        
+        let rank = "\(coin.marketCapRank)"
+        let rankStat = Statistic(title: "Rank", value: rank)
+        
+        let volume = "$" + (coin.totalVolume?.formattedWithAbbreviations() ?? "0")
+        let volumeStat = Statistic(title: "Volume", value: volume)
+        
+        return [
+            priceStat,
+            marketCapStat,
+            rankStat,
+            volumeStat
+        ]
+    }
+    
+    private func createAdditionalArray(coin: Coin, coinDetails: CoinDetail?) -> [Statistic] {
+        let high = coin.high24H?.toCurrency() ?? "n/a"
+        let highStat = Statistic(title: "24h High", value: high)
+        
+        let low = coin.low24H?.toCurrency() ?? "n/a"
+        let lowStat = Statistic(title: "24h Low", value: low)
+        
+        let priceChange = coin.priceChange24H.toCurrency() + "n/a"
+        let pricePercentChange = coin.priceChangePercentage24H
+        let priceChangeStat = Statistic(title: "24h Price Change", value: priceChange, percentageChange: pricePercentChange)
+        
+        let marketCapChange = "$" + (coin.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentageChange = coin.marketCapChangePercentage24H
+        let marketCapChangeStat = Statistic(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentageChange)
+        
+        let blockTime = coinDetails?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "n/a" : "\(blockTime)"
+        let blockStat = Statistic(title: "Block Time", value: blockTimeString)
+        
+        let hashing = coinDetails?.hashingAlgorithm ?? "n/a"
+        let hashingStat = Statistic(title: "Hashing Algorithm", value: hashing)
+        
+        return [
+            highStat,
+            lowStat,
+            priceChangeStat,
+            marketCapChangeStat,
+            blockStat,
+            hashingStat
+        ]
     }
 }
